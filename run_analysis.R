120cbe1a59bb8f3b3b107b16b276198ffde9cb6a
drsudhirpathak <32571627+drsudhirpathak@users.noreply.github.com>
2017-10-09 00:05:03 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,4 @@
 # Author: SUDHIR PATHAK
-# 1.Download the dataset if it does not already exist in the working directory
-#2.Load the activity and feature info
-#3.Loads both the training and test datasets, keeping only those columns which reflect a mean or standard deviation
-#4.Loads the activity and subject data for each dataset, and merges those columns with the dataset
-#5.Merges the two datasets
-#6.Converts the activity and subject columns into factors
-#7.Creates a tidy dataset that consists of the average (mean) value of each variable for each subject and activity pair.
-#The end result is shown in the file tidy.txt.
 library(reshape2)
 
 filename <- "getdata_dataset.zip"
@@ -39,7 +31,6 @@ train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
 trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
 trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
 train <- cbind(trainSubjects, trainActivities, train)
-
 test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
 testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
 testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
